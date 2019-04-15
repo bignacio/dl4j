840c7d0dee8d6c672a043d28f1e5538fc09ec5c6
Bira Ignacio <bira@viafoura.com>
2019-04-14 21:38:23 -0400

    setup grpc service

---
+++ word2vec-server/src/test/java/org/drizzle/ml/word2vec/handlers/RequestHandlerTest.java
@@ -0,0 +1,16 @@
+package org.drizzle.ml.word2vec.handlers;
+
+import org.junit.jupiter.api.Test;
+
+public class RequestHandlerTest {
+
+    @Test
+    public void doTest() {
+
+    }
+
+    @Test
+    public void doFail() {
+
+    }
+}
